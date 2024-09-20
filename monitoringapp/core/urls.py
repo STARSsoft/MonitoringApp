5a74bff0ad0ac529a39a9d9fc07b227b5475aa53
STARS <kazssr@gmail.com>
2024-09-20 17:09:44 +0500

    Создал и протестировал шаблоны страниц ввода цен, статистики и о нас

--- monitoringapp/core/urls.py
+++ monitoringapp/core/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.start_page, name='start_page'),  # Путь для стартовой страницы
+    path('priceadd/', views.price_add, name='price_add'),  # Путь для страницы ввода цен
+    path('statistics/', views.statistics, name='statistics'),   # Путь для страницы вывода статистики
+    path('about/', views.about_us, name='about_us'),    # Путь для страницы О нас
 ]
+
