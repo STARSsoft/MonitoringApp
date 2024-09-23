5298ffc60a5206d401b14a23092edf405a31c102
STARS <kazssr@gmail.com>
2024-09-23 20:12:44 +0500

    Изменил файл с УРЛ в основной папке проекта. После проверки вроде работает

--- monitoringapp/monitoringapp/urls.py
+++ monitoringapp/monitoringapp/urls.py
@@ -1,10 +1,30 @@
-# monitoringapp/urls.py
+# # monitoringapp/urls.py
+#
+# from django.contrib import admin
+# from django.urls import path, include
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('core.urls')),  # Включаем маршруты из приложения 'core'
+#
+# ]
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls.i18n import i18n_patterns
+from core import views
+from django.contrib.auth import views as auth_views
 
-urlpatterns = [
+# Маршруты с поддержкой многоязычности
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Включаем маршруты из приложения 'core'
-
-]
+    path('', views.start_page, name='start_page'),
+    path('priceadd/', views.price_add, name='price_add'),
+    path('statistics/', views.statistics, name='statistics'),
+    path('about/', views.about_us, name='about_us'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('profile/', views.profile_view, name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login-required/', views.login_required_view, name='login_required'),
+)
